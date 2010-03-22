cfa7cee3f093972e05e4ffff0ebfefd45f2820fa
bhagman <bhagman@roguerobotics.com>
2010-03-22 00:15:17 +0000

    SoftPWM: License updates, minor fixes

--- Arduino/libraries/SoftPWM/trunk/SoftPWM.h
+++ Arduino/libraries/SoftPWM/trunk/SoftPWM.h
@@ -1,4 +1,4 @@
-/* SoftPWM.h
+/* $Id$
 
   A Software PWM Library
   
@@ -11,18 +11,13 @@
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
+    This library is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-
-Version Modified By Date     Comments
-------- ----------- -------- --------
-0001    B Hagman    10/03/14 Initial coding (Pi Day!)
 
 *************************************************/
 
